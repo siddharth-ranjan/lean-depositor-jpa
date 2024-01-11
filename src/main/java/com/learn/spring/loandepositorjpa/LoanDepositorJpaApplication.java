34f57d1b1b97e1b69ce42577441529ddb8c2790f
Siddharth Ranjan <siddharthranjan0909@gmail.com>
2024-01-12 01:32:09 +0530

    Initial commit

---
+++ src/main/java/com/learn/spring/loandepositorjpa/LoanDepositorJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.loandepositorjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanDepositorJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanDepositorJpaApplication.class, args);
+    }
+
+}
